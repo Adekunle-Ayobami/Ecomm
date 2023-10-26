2d893ae1e270982280790d10c0890d86012b18e2
Adeniyi Ayobami <ayobamisamuel58@gmail.com>
2023-10-26 06:19:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// const products = [
+//   { id: 1, name: "Product 1", price: 10.99 },
+//   { id: 2, name: "Product 2", price: 15.99 },
+//   { id: 3, name: "Product 3", price: 20.99 },
+// ];
+
+// function displayProducts() {
+//   const productsSection = document.getElementById("products");
+//   productsSection.innerHTML = "";
+
+//   products.forEach((product) => {
+//     const productElement = document.createElement("div");
+//     productElement.className = "product";
+//     productElement.innerHTML = `
+//             <h3>${product.name}</h3>
+//             <p>Price: $${product.price}</p>
+//             <button onclick="addToCart(${product.id})">Add to Cart</button>
+//         `;
+//     productsSection.appendChild(productElement);
+//   });
+// }
+
+// function addToCart(productId) {
+
+// }
+
+// displayProducts();
